84f0a7c9f4f4870951e5db5745c475523dc78156
Sergei Dmitriev <iserdmi@gmail.com>
2025-10-18 23:26:06 +0400

    x

--- examples/vite/entry-client.tsx
+++ examples/vite/entry-client.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import { hydrate } from 'point0/client'
 import { pages } from './pages/index.js'
-import { createRoot, hydrateRoot } from 'react-dom/client'
 
 void hydrate({
-  pages: pages,
+  pages,
 })
