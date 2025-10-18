711ff886e7aab9f9596f3934d6dbf1947281ca19
Sergei Dmitriev <iserdmi@gmail.com>
2025-10-19 00:16:27 +0400

    x

--- examples/vite-hono/pages/index.ts
+++ examples/vite-hono/pages/index.ts
@@ -2,7 +2,7 @@ import type { PagesCollection } from 'point0/client'
 import { homeRoute, ideasRoute, ideaRoute } from '../shared/routes.js'
 
 export const pages: PagesCollection = [
-  [homeRoute, async () => await import('./home').then((m) => m.homePage)],
-  [ideasRoute, async () => await import('./ideas').then((m) => m.ideasPage)],
-  [ideaRoute, async () => await import('./idea').then((m) => m.ideaPage)],
+  [homeRoute, async () => await import('./home.js').then((m) => m.homePage)],
+  [ideasRoute, async () => await import('./ideas.js').then((m) => m.ideasPage)],
+  [ideaRoute, async () => await import('./idea.js').then((m) => m.ideaPage)],
 ]
