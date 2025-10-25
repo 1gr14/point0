2fbcfb66f22739dbcc35f2bf147a386c95e4e783
Sergei Dmitriev <iserdmi@gmail.com>
2025-10-25 21:40:39 +0400

    x

--- src/adapters/unhead/index.ts
+++ src/adapters/unhead/index.ts
@@ -3,6 +3,7 @@ import { createElement } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/promise-function-async
 export const Unhead = ({ children }: { children: React.ReactNode }) => {
+  // TODO: remove dead code by prvoide CLIENT_ONLY=true env variable
   if (typeof window !== 'undefined') {
     return createElement(UnheadProvider, { head: createHead(), children })
   } else {
