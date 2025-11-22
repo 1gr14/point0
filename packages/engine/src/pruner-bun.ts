2f26ccd21d47ee17aed14c036b43e3506689155a
Sergei Dmitriev <iserdmi@gmail.com>
2025-11-22 09:32:37 +0400

    x

--- packages/engine/src/pruner-bun.ts
+++ packages/engine/src/pruner-bun.ts
@@ -7,55 +7,60 @@ import { Walker } from './walker.js'
 export function prunerBunPlugin({
   customer,
   scope,
+  includedNodeModulesPrefixes: providedIncludedNodeModulesPrefixes = [],
 }: {
   customer: PruneCustomer
   scope: PointsScope | null
+  includedNodeModulesPrefixes: string[]
 }): BunPlugin {
   return {
     name: 'point0-pruner',
     setup(build) {
-      build.onLoad(
-        { filter: /^(?!.*node_modules).*\.[cm]?[tj]sx?$/ }, // JS/TS files, exclude node_modules
-        async (args) => {
-          let original: string | undefined
-          const filepath = args.path
-          try {
-            original = await Bun.file(filepath).text()
-            const walker = new Walker()
-            const transformed = await walker.prunePoint0Methods({
-              content: original,
-              fileAbs: filepath,
-              customer,
-            })
+      function escapeRegExp(str: string) {
+        return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+      }
+      const includedNodeModulesPrefixes = ['@point0/', ...providedIncludedNodeModulesPrefixes]
+      const prefixPattern = includedNodeModulesPrefixes.map((p) => escapeRegExp(p)).join('|')
+      const filter = new RegExp(`^(?!.*node_modules/(?!(${prefixPattern}))).*\\.[cm]?[tj]sx?$`)
+      build.onLoad({ filter }, async (args) => {
+        let original: string | undefined
+        const filepath = args.path
+        try {
+          original = await Bun.file(filepath).text()
+          const walker = new Walker()
+          const transformed = await walker.prunePoint0Methods({
+            content: original,
+            fileAbs: filepath,
+            customer,
+          })
 
-            if (transformed === original) {
-              return {
-                contents: original,
-                loader: guessLoader(filepath),
-              }
-            }
-
-            // const ms = new MagicString(original)
-            // ms.overwrite(0, original.length, transformed)
-
+          if (transformed === original) {
             return {
-              contents: transformed,
-              loader: guessLoader(filepath),
-              // sourcemap: ms.generateMap({
-              //   source: filepath,
-              //   includeContent: true,
-              //   hires: true,
-              // }),
-            }
-          } catch (e) {
-            console.error(e)
-            return {
-              contents: original ?? '',
+              contents: original,
               loader: guessLoader(filepath),
             }
           }
-        },
-      )
+
+          // const ms = new MagicString(original)
+          // ms.overwrite(0, original.length, transformed)
+
+          return {
+            contents: transformed,
+            loader: guessLoader(filepath),
+            // sourcemap: ms.generateMap({
+            //   source: filepath,
+            //   includeContent: true,
+            //   hires: true,
+            // }),
+          }
+        } catch (e) {
+          console.error(e)
+          return {
+            contents: original ?? '',
+            loader: guessLoader(filepath),
+          }
+        }
+      })
     },
   } satisfies BunPlugin
 }
--- packages/engine/src/pruner-vite.ts
+++ packages/engine/src/pruner-vite.ts
@@ -4,13 +4,27 @@ import type { Plugin } from 'vite'
 import type { PruneCustomer } from './walker.js'
 import { Walker } from './walker.js'
 
-export function prunerVitePlugin({ customer, scope }: { customer: PruneCustomer; scope: PointsScope | null }): Plugin {
+export function prunerVitePlugin({
+  customer,
+  scope,
+  includedNodeModulesPrefixes: providedIncludedNodeModulesPrefixes = [],
+}: {
+  customer: PruneCustomer
+  scope: PointsScope | null
+  includedNodeModulesPrefixes: string[]
+}): Plugin {
+  function escapeRegExp(str: string) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  }
+  const includedNodeModulesPrefixes = ['@point0/', ...providedIncludedNodeModulesPrefixes]
+  const prefixPattern = includedNodeModulesPrefixes.map((p) => escapeRegExp(p)).join('|')
+  const filter = new RegExp(`^(?!.*node_modules/(?!(${prefixPattern}))).*\\.[cm]?[tj]sx?$`)
   return {
     name: 'point0-pruner',
     enforce: 'pre',
     async transform(code, id, options) {
       const [filepath] = id.split('?', 1)
-      if (!/^(?!.*node_modules).*\.[cm]?[tj]sx?$/.test(filepath)) return null
+      if (!filter.test(filepath)) return null
 
       const walker = new Walker()
       const transformed = await walker.prunePoint0Methods({ content: code, fileAbs: filepath, customer })
