612486a216a88cf8faf6412bb39a6a1d65cfd933
Sergei Dmitriev <iserdmi@gmail.com>
2026-01-03 05:27:22 +0400

    x

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,7 +3,20 @@ import { fixImportsPlugin } from 'esbuild-fix-imports-plugin'
 
 // TODO: set best options
 const general = {
-  entry: ['src/index.ts'],
+  // entry: ['src/index.ts'],
+  entry: [
+    'src/client-server.ts',
+    'src/cookies-store.ts',
+    'src/index.ts',
+    'src/points-manager.ts',
+    'src/request.ts',
+    'src/response-effects.ts',
+    'src/router.tsx',
+    'src/super-store.ts',
+    'src/types.ts',
+    'src/unhead.ts',
+    'src/utils.ts',
+  ],
   clean: false,
   dts: true,
   sourcemap: true,
@@ -12,7 +25,7 @@ const general = {
   target: 'es2022',
   external: ['bun:test'],
   treeshake: true,
-  bundle: true,
+  bundle: false,
   esbuildPlugins: [fixImportsPlugin()],
   platform: 'node',
   tsconfig: './tsconfig.json',
