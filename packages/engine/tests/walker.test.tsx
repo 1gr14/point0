1d865a6989b0fa71a2db90f9e5523277eda8a793
Sergei Dmitriev <iserdmi@gmail.com>
2026-01-04 18:33:02 +0400

    x

--- packages/engine/tests/walker.test.tsx
+++ packages/engine/tests/walker.test.tsx
@@ -1,47 +1,76 @@
-import { afterEach, beforeEach, describe, it, expect } from 'bun:test'
+import { afterEach, beforeEach, describe, it, expect, beforeAll, afterAll } from 'bun:test'
 import * as nodeFs from 'node:fs'
 import * as nodePath from 'node:path'
 import { Walker } from '../src/walker.js'
 
-// TODO: move all tests to separate files in test dir and refactor it
+type MyFile = Bun.BunFile & { path: string; basename: string; importpath: string }
 
-describe('walker', () => {
-  const cwd = nodePath.join(__dirname, 'temp/walker')
+const tempDir = nodePath.join(__dirname, 'temp/walker')
 
-  const wrap = (callback: ({ file, path, walker }: { file: Bun.BunFile; path: string; walker: Walker }) => any) => {
-    return async () => {
-      const path = nodePath.join(cwd, Math.random().toString(36).substring(2, 15))
-      const file = Bun.file(path)
-      const walker = new Walker({ cwd })
-      try {
-        await callback({ file, path, walker })
-      } finally {
-        await file.delete()
-      }
+const createRandomFile = () => {
+  const basename = Math.random().toString(36).substring(2, 15)
+  const path = nodePath.join(tempDir, basename + '.tsx')
+  const importpath = './' + basename + '.js'
+  return Object.assign(Bun.file(path), { path, basename, importpath })
+}
+
+const helper = (callback: ({ files, walker }: { files: MyFile[]; walker: Walker }) => any, deleteFiles = true) => {
+  return async () => {
+    const walker = new Walker({ cwd: tempDir })
+    const files = [createRandomFile(), createRandomFile(), createRandomFile()]
+    try {
+      await callback({
+        files,
+        walker,
+      })
+    } finally {
+      await Promise.allSettled(
+        files.map(async (file) => {
+          if (deleteFiles) await file.delete()
+        }),
+      )
     }
   }
+}
 
-  beforeEach(() => {
-    nodeFs.mkdirSync(cwd, { recursive: true })
-  })
-
-  afterEach(() => {
-    nodeFs.rmSync(cwd, { recursive: true, force: true })
+describe('walker', () => {
+  beforeAll(() => {
+    nodeFs.rmSync(tempDir, { recursive: true, force: true })
+    nodeFs.mkdirSync(tempDir, { recursive: true })
   })
 
   it(
     'should collect points from file',
-    wrap(async ({ file, path, walker }) => {
+    helper(async ({ files: [file], walker }) => {
       await file.write(`import {Point0} from '@point0/core'
 export const root = Point0.create('server').root()
 export const page = root.lets('page', 'mypage').page(() => <div>Hello</div>)
     `)
-      const points = await walker.collectPointsFromFile({ fileAbs: path })
+      const points = await walker.collectPointsFromFile({ fileAbs: file.path })
       expect(points.collectedPoints).toMatchObject([
         {
           type: 'root',
           name: 'server',
         },
+        {
+          type: 'page',
+          name: 'mypage',
+        },
+      ])
+    }),
+  )
+
+  it(
+    'should collect points from file, when root in another file',
+    helper(async ({ files: [file0, file1], walker }) => {
+      await file0.write(`import {Point0} from '@point0/core'
+export const root = Point0.create('server').root()
+    `)
+      await file1.write(`import {root} from '${file0.importpath}'
+export const page = root.lets('page', 'mypage').page(() => <div>Hello</div>)
+    `)
+      const points = await walker.collectPointsFromFile({ fileAbs: file1.path })
+      expect(points.collectedPoints).toMatchObject([
         {
           type: 'page',
           name: 'mypage',
