b3b3f9d8052a750a6443e34b7f3f6dee9049d758
Sergei Dmitriev <iserdmi@gmail.com>
2026-02-11 19:46:19 +0400

    x

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -45,7 +45,6 @@ const _isTargetClient = (): boolean => {
   // Electron renderer process
   if (typeof process !== 'undefined' && (process as any).type === 'renderer') return true
 
-  // TODO: Electron main process in fact is client also (Yes it is client in point0 terminology, becouse it can send requests to server!)
   return false // Node.js, Bun, Deno, or other server runtimes
 }
 
@@ -53,6 +52,9 @@ const isTargetClient = (): boolean => {
   const fakeClient = superstore.isFakeClient()
   if (fakeClient) {
     return true
+  }
+  if (process.env.POINT0_TARGET) {
+    return process.env.POINT0_TARGET === 'client'
   }
   return _isTargetClient()
 }
