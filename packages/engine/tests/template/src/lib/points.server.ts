4f69416d2b06ddd97ed6682cc82e8084f8ce5db5
Sergei Dmitriev <iserdmi@gmail.com>
2026-02-12 23:38:48 +0400

    x

--- packages/engine/tests/template/src/lib/points.server.ts
+++ packages/engine/tests/template/src/lib/points.server.ts
@@ -1,3 +1,5 @@
 import type { PointsDefinition } from '@point0/core'
 import { root as root_0 } from './root.js'
-export default [root_0] as PointsDefinition<(typeof root_0)['Infer']['RequiredCtx']>
+export default [
+  root_0,
+] as PointsDefinition<typeof root_0['Infer']['RequiredCtx']>
