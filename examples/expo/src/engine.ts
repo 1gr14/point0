2d3ee5598ec1362388878b6ed25dfbd4dd26d0da
Sergei Dmitriev <iserdmi@gmail.com>
2026-04-16 01:20:23 +0400

    x

--- examples/expo/src/engine.ts
+++ examples/expo/src/engine.ts
@@ -3,12 +3,14 @@ import { Engine } from '@point0/engine'
 export const engine = Engine.create({
   file: import.meta.url,
   pointsGlob: ['**/*.{ts,tsx}'],
+  generate: { meta: './generated/point0/meta.ts' },
   server: {
     scope: 'root',
     port: 3000,
     entry: { main: './index.server.ts' },
-    generate: { points: './lib/points.server.ts' },
-    points: async () => await import('./lib/points.server'),
+    generate: { points: './generated/point0/points.server.ts' },
+    points: async () => await import('./generated/point0/points.server'),
+    devWatchGlob: ['**/*.{ts,tsx}', '!generated/point0/meta.ts'],
     outdir: '../dist/server',
   },
 })
